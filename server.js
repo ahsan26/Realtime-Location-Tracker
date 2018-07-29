06cb67cab083a02466c1aee96ef208912fead5b2
ahsan26 <ahsanusmanabduljabbar@gmail.com>
2018-07-29 23:12:27 +0500

    make it better

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
+
+app.use(express.static('public'))
+
+app.set('port', 19000);
+
+const server = app.listen(app.get('port'), function () {
+    console.log('Server is started on: ', app.get('port'));
+});
+
+const io = socket(server);
+
+io.on('connection', function (socket) {
+    console.log('Client Connected', socket.id);
+    socket.on('location', function (data) {
+        io.sockets.emit('userLocation', data);
+    });
+});
